28d43137c6fb1f9f42d3158f02e627da11f6a777
baldo <baldo@toppoint.de>
2015-09-12 10:52:41 +0200

    Test

--- ffnord-con-2015-2/node.lua
+++ ffnord-con-2015-2/node.lua
@@ -1,6 +1,6 @@
 gl.setup(1280, 720)
 
 function node.render()
-    resource.render_child("room"):draw(0, 0, 1280, 720)
---    resource.render_child("logo"):draw(0, 0, 1280, 720)
+--    resource.render_child("room"):draw(0, 0, 1280, 720)
+    resource.render_child("logo"):draw(0, 0, 1280, 720)
 end
