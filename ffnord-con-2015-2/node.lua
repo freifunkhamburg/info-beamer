bca6e59cadf174bad1fdb51bf5cff68913c33aae
baldo <baldo@toppoint.de>
2015-09-12 12:10:52 +0200

    Animation aktiv

--- ffnord-con-2015-2/node.lua
+++ ffnord-con-2015-2/node.lua
@@ -1,6 +1,9 @@
 gl.setup(1280, 720)
 
 function node.render()
-    resource.render_child("room"):draw(0, 0, 1280, 720)
+    resource.render_child("31c3"):draw(0, 0, 1280, 720)
+    resource.render_child("29c3"):draw(0, 0, 1280, 720)
+--    resource.render_child("room"):draw(0, 0, 1280, 720)
 --    resource.render_child("logo"):draw(0, 0, 1280, 720)
 end
+
